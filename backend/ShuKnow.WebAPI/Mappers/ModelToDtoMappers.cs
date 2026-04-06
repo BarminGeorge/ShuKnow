bded67cce2fb6007a7e91e2e7036bdcb49779fec
Ravil <182143436+hereexe@users.noreply.github.com>
2026-04-06 16:09:00 +0500

    feat: logic ChatNotif

--- backend/ShuKnow.WebAPI/Mappers/ModelToDtoMappers.cs
+++ backend/ShuKnow.WebAPI/Mappers/ModelToDtoMappers.cs
@@ -1,5 +1,9 @@
-﻿using ShuKnow.Domain.Entities;
+using ShuKnow.Domain.Entities;
 using ShuKnow.WebAPI.Dto.Auth;
+using ShuKnow.WebAPI.Dto.Chat;
+using ShuKnow.WebAPI.Dto.Files;
+using ShuKnow.WebAPI.Dto.Folders;
+using File = ShuKnow.Domain.Entities.File;
 
 namespace ShuKnow.WebAPI.Mappers;
 
@@ -8,5 +12,45 @@ public static class ModelToDtoMappers
     public static UserDto ToDto(this User user)
     {
         return new UserDto(user.Id, user.Login);
+    }
+
+    public static ChatMessageDto ToDto(this ChatMessage message)
+    {
+        return new ChatMessageDto(
+            message.Id,
+            (Dto.Enums.ChatMessageRole)message.Role,
+            message.Content,
+            message.Index,
+            null);
+    }
+
+    public static FileDto ToDto(this File file, string folderName = "")
+    {
+        return new FileDto(
+            file.Id,
+            file.FolderId,
+            folderName,
+            file.Name,
+            file.Description,
+            file.ContentType,
+            file.SizeBytes,
+            file.Version,
+            file.ChecksumSha256,
+            file.SortOrder,
+            file.CreatedAt);
+    }
+
+    public static FolderDto ToDto(this Folder folder)
+    {
+        return new FolderDto(
+            folder.Id,
+            folder.Name,
+            folder.Description,
+            folder.Emoji,
+            folder.ParentFolderId,
+            folder.SortOrder,
+            0,
+            false,
+            null);
     }
 }
--- backend/ShuKnow.WebAPI/Services/ChatNotificationService.cs
+++ backend/ShuKnow.WebAPI/Services/ChatNotificationService.cs
@@ -1,58 +1,61 @@
+using Microsoft.AspNetCore.SignalR;
 using ShuKnow.Application.Interfaces;
 using ShuKnow.Domain.Entities;
+using ShuKnow.WebAPI.Events;
+using ShuKnow.WebAPI.Hubs;
+using ShuKnow.WebAPI.Mappers;
+
 using File = ShuKnow.Domain.Entities.File;
 
 namespace ShuKnow.WebAPI.Services;
 
-public class ChatNotificationService(ICurrentConnectionService currentConnection) : IChatNotificationService
+public class ChatNotificationService(
+    ICurrentConnectionService currentConnection,
+    IHubContext<ChatHub> hubContext) : IChatNotificationService
 {
+    private IClientProxy Client => hubContext.Clients.Client(currentConnection.connectionId);
+
     public Task SendProcessingStartedAsync(UserAction action, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnProcessingStarted), new ProcessingStartedEvent(action.OperationId), ct);
 
     public Task SendMessageChunkAsync(ChatMessage message, string chunk, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnMessageChunk), new MessageChunkEvent(Guid.Empty, message.Id, chunk), ct);
 
     public Task SendMessageCompletedAsync(ChatMessage message, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnMessageCompleted), message.ToDto(), ct);
 
     public Task SendClassificationResultAsync(IReadOnlyCollection<ActionItem> decisions, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var operationId = decisions.FirstOrDefault()?.ActionId ?? Guid.Empty;
+        var decisionsDto = decisions.Select(x => new ClassificationDecisionDto(
+            FileName: string.Empty, 
+            TargetFolderName: string.Empty, 
+            TargetFolderId: null, 
+            IsNewFolder: false)).ToList();
+
+        return SendEventAsync(nameof(ChatHub.OnClassificationResult), new ClassificationResultEvent(operationId, decisionsDto), ct);
     }
 
     public Task SendFileCreatedAsync(File file, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnFileCreated), file.ToDto(), ct);
 
     public Task SendFileMovedAsync(ActionItemFileMoved movedFile, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnFileMoved), new FileMovedEvent(movedFile.FileId, movedFile.FromFolderId, movedFile.ToFolderId), ct);
 
     public Task SendFolderCreatedAsync(Folder folder, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnFolderCreated), folder.ToDto(), ct);
 
     public Task SendProcessingCompletedAsync(UserAction action, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnProcessingCompleted), 
+            new ProcessingCompletedEvent(action.OperationId, action.Id, action.Summary, action.ItemCount, 0), ct);
 
     public Task SendProcessingFailedAsync(UserAction action, string error, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnProcessingFailed), 
+            new ProcessingFailedEvent(action.OperationId, error, ProcessingErrorCode.InternalError), ct);
 
     public Task SendProcessingCancelledAsync(ChatMessage cancellationRecord, CancellationToken ct = default)
-    {
-        throw new NotImplementedException();
-    }
+        => SendEventAsync(nameof(ChatHub.OnProcessingCancelled), new ProcessingCancelledEvent(cancellationRecord.Id), ct);
+
+    private Task SendEventAsync<TEvent>(string methodName, TEvent @event, CancellationToken ct)
+        => Client.SendAsync(methodName, @event, cancellationToken: ct);
 }
