50ea789a1f00ac3e5d88ce246e916bbcf934c279
Feytox <73577753+feytox@users.noreply.github.com>
2026-04-08 19:54:24 +0500

    refactor(backend): refactor ToBase64Async Stream extension

--- backend/ShuKnow.Infrastructure/Misc/StreamExtensions.cs
+++ backend/ShuKnow.Infrastructure/Misc/StreamExtensions.cs
@@ -9,14 +9,14 @@ public static class StreamExtensions
     public static async Task<Result<string>> ToBase64Async(this Task<Result<Stream>> resultTask,
         CancellationToken ct = default)
     {
-        var streamResult = await resultTask;
-        if (!streamResult.IsSuccess)
-            return streamResult.Map();
-
-        await using var stream = streamResult.Value;
-        return await stream.ToBase64Async(ct);
+        return await resultTask
+            .MapAsync(async streamValue =>
+            {
+                await using var stream = streamValue;
+                return await stream.ToBase64Async(ct);
+            });
     }
-    
+
     public static async Task<string> ToBase64Async(this Stream stream, CancellationToken ct = default)
     {
         if (stream == null)
@@ -25,7 +25,8 @@ public static class StreamExtensions
             throw new InvalidOperationException("Stream must be readable.");
 
         await using var outputStream = new MemoryStream();
-        await using var cryptoStream = new CryptoStream(outputStream, new ToBase64Transform(), CryptoStreamMode.Write, leaveOpen: true);
+        await using var cryptoStream = new CryptoStream(
+            outputStream, new ToBase64Transform(), CryptoStreamMode.Write, leaveOpen: true);
 
         await stream.CopyToAsync(cryptoStream, 81920, ct);
         await cryptoStream.FlushFinalBlockAsync(ct);
